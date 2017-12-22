a8e51123179465c6714977e103373e32f440ce24
Chesley Stephens <chesbot@gmail.com>
2017-12-21 20:36:35 -0600

    Initial Commit

---
+++ CaffeineKick/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CaffeineKick
+//
+//  Created by Chesley Stephens on 12/21/17.
+//  Copyright © 2017 Nibbis. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
